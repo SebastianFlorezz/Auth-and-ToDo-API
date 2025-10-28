862488e3ede39edc913ed237c915e051d99e8979
Sebastian <sebastianflorezbuelvas@gmail.com>
2025-10-28 09:22:54 -0500

    good

--- app.js
+++ app.js
@@ -12,13 +12,20 @@ app.listen(PORT, (error) => {
         console.error("Failed to start sever:",error)
     }
 
-    console.log("Server initialized in PORT: ", PORT)
+    console.log("Server initialized in PORT: ", PORT || 5000 )
+    console.log(`Documentation: http://localhost:${PORT || 5000}/api-docs/`)
 })
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+//swagger io 
+
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./docs/swagger.yaml")
+
 
 app.use("/users", userRoutes)
 app.use("/tasks", taskRoutes)
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
